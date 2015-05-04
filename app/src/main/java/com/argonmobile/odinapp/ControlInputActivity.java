2985dacf1e1a9b4688e69c2018e1730fd11ea0d7
yuan.xu <emotionfxxk@gmail.com>
2015-05-04 18:17:19 +0800

    Implement ball control activity

--- app/src/main/java/com/argonmobile/odinapp/ControlInputActivity.java
+++ app/src/main/java/com/argonmobile/odinapp/ControlInputActivity.java
@@ -2,16 +2,28 @@ package com.argonmobile.odinapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.argonmobile.odinapp.protocol.ballcontrol.BallActionFactory;
+import com.argonmobile.odinapp.protocol.ballcontrol.BallControlAction;
+import com.argonmobile.odinapp.protocol.command.RequestFactory;
+import com.argonmobile.odinapp.protocol.connection.ConnectionManager;
+import com.argonmobile.odinapp.protocol.connection.ControlConnection;
 
-public class ControlInputActivity extends ActionBarActivity {
 
+public class ControlInputActivity extends ActionBarActivity implements BallActionFactory.ActionListener {
+    private final static String TAG = "ControlInputActivity";
+    private BallActionFactory ballActionFactory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_input);
+        if(ballActionFactory == null) {
+            ballActionFactory = new BallActionFactory(this);
+        }
+        ballActionFactory.setActionListener(this);
     }
 
 
@@ -35,5 +47,27 @@ public class ControlInputActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ballActionFactory.start();
+    }
+
+    @Override
+    public void onStop() {
+        ballActionFactory.stop();
+        super.onStop();
+    }
+
+    @Override
+    public void onAction(BallControlAction action) {
+        Log.i(TAG, "onAction " + action);
+        final ControlConnection cc = ConnectionManager.defaultManager.getControlConnection();
+        if(cc != null) {
+            //
+            cc.sendCommand(RequestFactory.createControlBallRequest(action.action, (byte)0x01));
+        }
     }
 }
